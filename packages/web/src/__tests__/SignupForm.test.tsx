5930e4a976f18469160fbfcc05b4ad9b53f16edb
Ivan Roldan Lusich <ivanrl2010@gmail.com>
2020-06-15 16:42:14 -0300

    add signup form test

--- packages/web/src/__tests__/SignupForm.test.tsx
+++ packages/web/src/__tests__/SignupForm.test.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { render, fireEvent, screen } from "@testing-library/react";
+import {
+  render,
+  fireEvent,
+  screen,
+  waitForElement,
+} from "@testing-library/react";
 import { act } from "react-dom/test-utils";
 import userEvent from "@testing-library/user-event";
 import { ActionTypes } from "@reddit-clone/controller";
 import SignupFormConnector from "../modules/signupForm/SignupFormConnector";
-/*
-test('', () => {})
+import faker from "faker";
+
 const mockSelector = jest.fn();
 const mockDispatch = jest.fn();
 const mockCloseForm = jest.fn();
@@ -21,32 +26,34 @@ beforeEach(() => {
 
 describe("form functions correctly", () => {
   test("signup function is called", async () => {
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+    const username = faker.internet.userName();
+
     const emailInput = screen.getByAltText("email input");
     const nextButton = screen.getByRole("button", { name: "NEXT" });
     await act(async () => {
-      await userEvent.type(emailInput, "thisIsANewEmail@gmail.com");
+      await userEvent.type(emailInput, email);
       fireEvent.click(nextButton);
-      fireEvent.click(nextButton);
-      fireEvent.click(nextButton);
+      await waitForElement(() => screen.getByAltText("username input"));
     });
 
     const usernameInput = screen.getByAltText("username input");
     const passwordInput = screen.getByAltText("password input");
     const signupButton = screen.getByRole("button", { name: "SIGN UP" });
     await act(async () => {
-      await userEvent.type(usernameInput, "ivanrlnew");
-      await userEvent.type(passwordInput, "73442332Ivan");
+      await userEvent.type(usernameInput, username);
+      await userEvent.type(passwordInput, password);
       fireEvent.click(signupButton);
     });
 
     expect(mockDispatch).toHaveBeenCalledWith({
-      type: ActionTypes.SIGNIN_USER,
+      type: ActionTypes.SIGNUP_USER,
       payload: {
-        username: "ivanrlnew",
-        password: "73442332Ivan",
-        email: "thisIsANewEmail@gmail.com",
+        username,
+        password,
+        email,
       },
     });
   });
 });
-*/
