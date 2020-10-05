663a68aaf890deab553a990aa8d3682d49c1b85b
ivaanrl <ivanrl2010@gmail.com>
2020-10-05 17:24:59 -0300

    add dev.ts to git

---
+++ packages/server/config/dev.ts
@@ -0,0 +1,12 @@
+export const devKeys = {
+  cookieSecret: "ThisIsYourDevCookieSecret",
+  AWS_SECRET_KEY: "",
+  AWS_ACCESS_KEY: "",
+  AWS_BUCKET: "",
+};
+
+export const devDbCredentials = {
+  DB_NAME: "reddit",
+  DB_USER: "ivanrl",
+  DB_PASSWORD: "73442332",
+};
